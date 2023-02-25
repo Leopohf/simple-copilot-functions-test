178c873bcb525ccc30369f7aa3e5b8297e83b0d0
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-02-25 01:44:09 -0500

    test using copilot with ts

---
+++ TS/mathFunctions.ts
@@ -0,0 +1,35 @@
+// function to add two variables
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// function to subtract two variables
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+// function to multiply two variables
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+// function to divide two variables
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+// function to calculate the remainder of two variables
+
+function remainder(a: number, b: number): number {
+  return a % b;
+}
+
+// function to calculate the power of two variables
+
+function power(a: number, b: number): number {
+  return a ** b;
+}
