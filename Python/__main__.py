78ac6c0d1800d4a48b8b1dc9954c577cf5baa15b
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-03-03 18:41:15 -0500

    adding copilot autogenerated math functions

---
+++ Python/__main__.py
@@ -0,0 +1,35 @@
+# function to add two variables
+def add(a, b):
+    return a + b
+
+# function to subtract two variables
+def subtract(a, b):
+    return a - b
+
+# function to multiply two variables
+def multiply(a, b):
+    return a * b
+
+# function to divide two variables
+def divide(a, b):
+    return a / b
+
+# function to calculate the remainder of two variables
+def remainder(a, b):
+    return a % b
+
+# function to calculate the power of two variables
+def power(a, b):
+    return a ** b
+
+# function to calculate the square root of a variable
+def square_root(a):
+    return a ** 0.5
+
+# function to calculate the absolute value of a variable
+def absolute_value(a):
+    return abs(a)
+
+# function to calculate the hypothenuse of a triangle
+def hypothenuse(a, b):
+    return (a ** 2 + b ** 2) ** 0.5
